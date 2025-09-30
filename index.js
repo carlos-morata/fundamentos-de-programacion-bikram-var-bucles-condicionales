0523cb03bf8e8e868a359abbddf845b0b785189c
CarlosMoratinos5 <moratinos.carlos5@gmail.com>
2025-09-30 20:34:36 +0200

    Conectar github

---
+++ index.js
@@ -0,0 +1,31 @@
+// 1 - VARIABLES
+let variableSinValor;
+
+// 2
+let booleano1 = true;
+let booleano2 = false;
+
+// 3
+const PI = 3.14;
+
+// 4
+const TAU = PI * 2;
+
+// 5
+let booleanoAnd = booleano1 && booleano2;
+
+// 6
+let booleanoNot = !booleano1;
+
+// 7
+let booleanoMix0 = (booleano1 || booleano2) && (booleano1 || (!booleano1 && !booleano2));
+
+// 8 - OPERADORES
+let incrementarDesp = 2;
+let resultadoDesp = incrementarDesp++;
+
+// 9
+let incrementarAntes = 2;
+let resultadoAntes = ++incrementarAntes;
+
+// 10 - BUCLES
