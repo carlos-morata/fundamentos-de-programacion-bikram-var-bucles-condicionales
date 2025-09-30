ab7786379c2ef388f5993c620cfb8538d2946fd5
CarlosMoratinos5 <moratinos.carlos5@gmail.com>
2025-09-30 21:01:06 +0200

    add/ej/10,11,12

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // 1 - VARIABLES
-let variableSinValor;
+let variableSinValor = "";
 
 // 2
 let booleano1 = true;
@@ -29,3 +29,24 @@ let incrementarAntes = 2;
 let resultadoAntes = ++incrementarAntes;
 
 // 10 - BUCLES
+let contarHasta10_2 = 2;
+for(let i = 0; i === 10; i++) {
+    console.log(contarHasta10_2);
+}
+
+// 11
+// let postI = 0;
+// let postJ = 0;
+
+// for(let i = 0; i < 11; postI++) {
+
+// }
+
+// 12
+let sumaPares = 0;
+for(let i = 0; i < 10; i++) {
+    if(i % 2 == 0) {
+        
+    }
+    console.log(sumaPares);
+}
