8faa4ac5d72b6c7f0eeab3623fea47ee7615ff31
CarlosMoratinos5 <moratinos.carlos5@gmail.com>
2025-10-03 00:01:45 +0200

    add/ej/completado

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // 1 - VARIABLES
-let variableSinValor = "sin valor";
+let variableSinValor;
 
 // 2
 let booleano1 = true;
@@ -29,33 +29,34 @@ let incrementarAntes = 2;
 let resultadoAntes = ++incrementarAntes;
 
 // 10 - BUCLES
-let contarHasta10_2 = 2;
-for(let i = 0; i === 10; i++) {
-    console.log(contarHasta10_2);
+let contarHasta10_2 = 0;
+for(let i = 0; i < 10; i++) {
+    contarHasta10_2++
 }
 
 // 11
-// let postI = 0;
-// let postJ = 0;
+let postI = 0;
+let postJ = 0;
 
-// for(let i = 0; i < 11; postI++) {
-
-// }
+for(let i = 0; i < 11; i++) {
+    postI = postI + postJ++;
+}
+console.log(postI)
 
 // 12
-// let sumaPares = 0;
-// for(let i = 0; i < 10; i++) {
-//     if(i % 2 == 0) {
-        
-//     }
-//     console.log(sumaPares);
-// }
+let sumaPares = 0;
+for(let i = 0; i < 10; i++) {
+    if(i % 2 == 0) {
+        sumaPares += i
+    }
+    console.log(sumaPares);
+}
 
 // 13 VARIABLES
 let variableValorNumerico = 51;
 
 // 14
-const MiNombre = Carlos;
+const MiNombre = "Carlos";
 
 // 15
 const MiNumeroFav = 5;
@@ -67,10 +68,10 @@ let booleanoOr = booleano1 || booleano2;
 let booleanoMix1 = (booleano1 && (TAU/2 === PI)) || (variableValorNumerico >= MiNumeroFav);
 
 // 18
-let seisNoEsNueve = 6 == 9;
+let seisNoEsNueve = 6 != 9;
 
 // 19
-let booleanoMix2 = variableValorNumerico < (MiNumeroFav * TAU);
+let booleanoMix2 = (variableValorNumerico > 0) || (variableValorNumerico < - (MiNumeroFav * TAU))
 
 // 20 OPERADORES
 let valorSuma = MiNumeroFav + variableValorNumerico;
@@ -79,7 +80,30 @@ let valorSuma = MiNumeroFav + variableValorNumerico;
 let valorResta = MiNumeroFav - variableValorNumerico;
 
 // 22
-let valorMultiplicación = MiNumeroFav * variableValorNumerico;
+let valorMultiplicacion  = MiNumeroFav * variableValorNumerico;
 
 // 23
-let valorDivisión = MiNumeroFav / 3;
+let valorDivision = MiNumeroFav / 3;
+
+// 24 BUCLES
+let contarHasta10 = 0;
+
+while(contarHasta10 < 10) {
+    contarHasta10
+}
+
+// 25
+let preI = 0;
+let preJ = 0;
+
+for(let i = 0; i < 11; i++) {
+    preI += ++preJ
+}
+
+// 26
+let sumaImpares = 0;
+for(let i = 0; i < 10; i++) {
+    if(i % 2 !== 0) {
+       sumaImpares += i
+    }
+}
